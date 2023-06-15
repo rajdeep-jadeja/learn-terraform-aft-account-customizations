64b8bbd4466f15ef7d56938f5e0bf23d4075e975
Rajdeepsinh Jadeja (Cloud Infrastructure) <rajdeepsinh.jadeja@intuitive.cloud>
2023-06-16 00:25:04 +0530

    delete

--- sandbox/terraform/vpc.tf
+++ sandbox/terraform/vpc.tf
@@ -1,9 +1,9 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-resource "aws_vpc" "raj" {
-  cidr_block       = "10.0.0.0/16"
-  tags = {
-    Name = "raj"
-  }
-}
+# resource "aws_vpc" "main" {
+#   cidr_block = "10.0.0.0/16"
+# }
+# resource "aws_vpc" "raj" {
+#   cidr_block       = "10.0.0.0/16"
+#   tags = {
+#     Name = "raj"
+#   }
+# }
